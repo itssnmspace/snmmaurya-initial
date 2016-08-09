22f00f359affc78d1154a74c2c977123e767be78
Snm Maurya <snmmaurya@gmail.com>
2016-08-09 23:52:24 +0530

    cap setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,21 @@
 # config valid only for current version of Capistrano
 lock '3.6.0'
 
-set :application, 'snmmaurya'
-set :repo_url, 'git@github.com:snmspace/snmmaurya.git'
+# Change these
+server '138.68.8.26',  roles: [:web, :app, :db], primary: true
 
-ask :branch, "master"
-
-set :use_sudo, true
-set :bundle_binstubs, nil
-set :linked_files, fetch(:linked_files, []).push('config/database.yml')
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-set :bundle_flags, "--deployment --quiet --local"
-
-
+set :repo_url,        'git@github.com:snmspace/snmmaurya.git'
+set :application,     'snmmaurya'
+set :user,            'snmspace'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
-
+# Don't change these unless you know what you're doing
+set :pty,             true
+set :use_sudo,        false
+set :stage,           :production
+set :deploy_via,      :remote_cache
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
@@ -27,10 +26,16 @@ set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, false  # Change to true if using ActiveRecord
 
+## Defaults:
+# set :scm,           :git
+# set :branch,        :master
+# set :format,        :pretty
+# set :log_level,     :debug
+# set :keep_releases, 5
 
-after 'deploy:publishing', 'deploy:restart'
-
-
+## Linked Files & Directories (Default None):
+# set :linked_files, %w{config/database.yml}
+# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
@@ -43,8 +48,6 @@ namespace :puma do
 
   before :start, :make_dirs
 end
-
-
 
 namespace :deploy do
   desc "Make sure local git is in sync with remote."
@@ -77,10 +80,8 @@ namespace :deploy do
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
   after  :finishing,    :restart
-end  
+end
 
-# namespace :deploy do
-#   task :restart do
-#     # invoke 'unicorn:reload'
-#   end
-# end
+# ps aux | grep puma    # Get puma pid
+# kill -s SIGUSR2 pid   # Restart puma
+# kill -s SIGTERM pid   # Stop puma
